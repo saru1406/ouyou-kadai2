4183db3fc67186311a784adf894ca45e3e16f7c5
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-09-21 08:18:07 +0000

    finsh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   private
 
   def after_sign_in_path_for(resource)
-    book_path
+    books_path
   end
 
   def after_sign_out_path_for(resource)
