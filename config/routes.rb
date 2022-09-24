7890ff418b4503cfab1fbbc7d2e8a7925c885f03
EC2 Default User <ec2-user@ip-172-31-8-59.ap-northeast-1.compute.internal>
2022-09-24 07:36:12 +0000

    ouyou5

--- config/routes.rb
+++ config/routes.rb
@@ -8,16 +8,14 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     
     resources :book_comments, only: [:create]
-    
-    
-    
   end
+  
   resources :users, only: [:index,:show,:edit,:update]do
    resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   
-  
+  get "search" => "searches#search"
 
 end
